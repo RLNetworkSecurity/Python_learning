44fc6b84c097bca608713a079612a6dfa43a8114
Cyprotect <39667645+Cyprotect@users.noreply.github.com>
2020-06-30 14:26:15 +0100

    Update Celsuis_farenheit_temperature_changer.py

--- Celsuis_farenheit_temperature_changer.py
+++ Celsuis_farenheit_temperature_changer.py
@@ -1,5 +1,5 @@
 DEGC = "\u2103"
 DEGF = "\u2109"
-degrees_celsius = float(input("input your temperature in {DEGC}"))
+degrees_celsius = float(input(f"input your temperature in {DEGC}"))
 degrees_fahrenheit = (degrees_celsius * 9/5) + 32
 print(f"{degrees_celsius:.1f}{DEGC} is {degrees_fahrenheit:.1f}{DEGF}")
