47150e0d1f24424315b91ebc1ae62a5063edb166
Cyprotect <39667645+Cyprotect@users.noreply.github.com>
2020-06-30 14:10:00 +0100

    Create Celsuis_farenheit_temperature_changer.py

---
+++ Celsuis_farenheit_temperature_changer.py
@@ -0,0 +1,4 @@
+degrees_celsuis = input("input your temperature in \u2103")
+degrees_celsuis_float = float(degrees_celsuis)
+degrees_farenheit = (degrees_celsuis_float * 9/5) + 32
+print(f"The temperature is {degrees_farenheit:.1f}\u2109")
