e495600cead2d0fd83546c5219a02e23a898d0de
Cyprotect <39667645+Cyprotect@users.noreply.github.com>
2020-07-01 14:08:12 +0100

    Update slicing_lists.py

--- slicing_lists.py
+++ slicing_lists.py
@@ -2,3 +2,12 @@
 names = ['frank', ['jess', 'ron'], 'phil']
 names[1][1][0]
 #returns the r from ron because its the second part is the sub list. 
+
+
+
+# Split and join altering a list
+name = "Bat Man"
+parts = name.spit(" ")
+parts[0] = "Toad"
+newname = " ".join(parts)
+print(newname)
