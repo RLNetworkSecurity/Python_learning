030003d31f43457425034685b5f7888b9ac5eea1
Cyprotect <39667645+Cyprotect@users.noreply.github.com>
2020-07-01 12:08:44 +0100

    Update string_techinques_file_open.py

--- string_techinques_file_open.py
+++ string_techinques_file_open.py
@@ -6,13 +6,11 @@ print("Hello","Frank",sep="@")
 
 print("Hello","Frank",sep=",")
 
-
 #Default print end
 print("Hello", end="\n")
 
 #no end to print
 print("Hello", end="")
-
 
 #Send to a file - quick and dirty way, useful for logging
 #w will write to the file, a will append
@@ -24,6 +22,12 @@ myfile = open("c:\\mydata.txt","w")
 #print a raw string
 print(r"c:\newdata.txt")
 
-
 #open explicit file for append
 open(r"c:\newdata.txt","a")
+
+#find and replace example
+link = "https://cyprotect.co.uk"
+link.replace("https","ftp")
+
+# multiple a string
+NQQ * 4
