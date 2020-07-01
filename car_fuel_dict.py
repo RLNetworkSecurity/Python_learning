6f24f681d3726ba91cfd99885a4c23f993493c80
Cyprotect <39667645+Cyprotect@users.noreply.github.com>
2020-07-01 16:17:13 +0100

    Create car_fuel_dict.py

---
+++ car_fuel_dict.py
@@ -0,0 +1,15 @@
+#variables_fixed
+UPPER_LIMIT = 1.3
+UPPER_RATE = 35
+LOWER_RATE = 25
+# input variables
+miles_traveled = float(input("Input your miles traveled: "))
+engine_size = (input(f"Is your engine size above {UPPER_LIMIT}, answer yes or no: "))
+
+engine_size_dictionary = {"yes":35, "no":120,}
+fuel_allowance = int(engine_size_dictionary[engine_size])
+#calculation
+fuel_claim = miles_traveled * fuel_allowance/100
+
+#output
+print(f"{miles_traveled:.2f} for {engine_size} results £{fuel_claim:.2f}")
