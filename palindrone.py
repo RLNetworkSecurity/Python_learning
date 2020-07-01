e9dbc012a39e907d90659cb10e0078d9ff366b3d
Cyprotect <39667645+Cyprotect@users.noreply.github.com>
2020-07-01 14:50:09 +0100

    Update palindrone.py

--- palindrone.py
+++ palindrone.py
@@ -1,10 +1,11 @@
-# input variables
 import re
+# input variables
 input_word = input("Input your potential palindrome: ")
+#prep words
 clean_word = re.sub('[^A-Za-z]+', '', input_word)
 reverse_word = clean_word[::-1]
 
-# if statement engine size and fuel calculation
+# if statement to check reverse word = input word
 if new_word == reverse_word:
     print(f" your word{input_word} is a palindrome as shown: {clean_word}")
 else:
