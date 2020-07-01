818424cb76e8eeb5a1e1f872018a6bc619c60ab6
Cyprotect <39667645+Cyprotect@users.noreply.github.com>
2020-07-01 14:00:09 +0100

    Update palindrone.py

--- palindrone.py
+++ palindrone.py
@@ -6,6 +6,6 @@ reverse_word = clean_word[::-1]
 
 # if statement engine size and fuel calculation
 if new_word == reverse_word:
-    print(f" your word{input_word} is a palindrome as shown: {new_word}")
+    print(f" your word{input_word} is a palindrome as shown: {clean_word}")
 else:
     print(f" your word {input_word} is not a palindrome as shown: {clean_word}")
