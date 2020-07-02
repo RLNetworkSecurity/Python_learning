bedc6a215d2e85ff19119d0ac5d45979d373cd89
Cyprotect <39667645+Cyprotect@users.noreply.github.com>
2020-07-02 14:41:54 +0100

    Create for_loop_example.py

---
+++ for_loop_example.py
@@ -0,0 +1,14 @@
+#for loop examples
+
+for i in range(5):
+    print("hello")
+
+
+cats = ["Frank", "Phil", "Jess"]
+for cat in cats:
+    print(cat)
+
+
+name = "Frank"
+for letter in name:
+    print(letter)
