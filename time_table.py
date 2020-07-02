bb5ac57713a28138f35eb920b9ede9e09f78987d
Cyprotect <39667645+Cyprotect@users.noreply.github.com>
2020-07-02 15:18:25 +0100

    Update time_table.py

--- time_table.py
+++ time_table.py
@@ -1,11 +1,14 @@
 #while loop example
 
-table_number = int(input("Input your miles traveled: ")) 
+table_number = int(input("Input yoru times table: ")) 
 number_rows = int(input("Input row_number: "))
 
+#display heading
+print(f"{table_number} times table")
+print("-"*60)
 
 #for loop examples
 
-for table_number in range(number_rows):
-    answer = table_number * number_rows
-    print(f"{table_number} * {number_rows} = {answer}")
+for row in range(number_rows+1):
+    answer = table_number * row
+    print(f"{row:3} * {table_number} = {answer:4}")
