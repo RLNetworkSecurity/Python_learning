ee501da2b5af26b88500054ab24cb80407a23d1d
Cyprotect <39667645+Cyprotect@users.noreply.github.com>
2020-07-03 11:52:52 +0100

    Update DIMENSION.py

--- DIMENSION.py
+++ DIMENSION.py
@@ -1,4 +1,4 @@
 # function for DIMENSION
 
 def get_surface_area(*, height:float=2,width:float=5,length:float=10) -> float:
-        return float(length*height*2 + width*height*2)
+    return float(length*height*2 + width*height*2)
