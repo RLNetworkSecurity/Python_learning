9f660ce1200c6bdca957f88e224a9e7a7d13ed2b
Cyprotect <39667645+Cyprotect@users.noreply.github.com>
2020-07-03 13:59:14 +0100

    Create file_work.py

---
+++ file_work.py
@@ -0,0 +1,16 @@
+#load the file
+myfile = open("days.txt", "r")
+
+#read the file
+
+#everything
+mydata = myfile.read()
+
+#13 characters
+mydata = myfile.read(13)
+
+# list of the data
+mydata = myfile.read.lines()
+
+#print the file
+print(mydata)
